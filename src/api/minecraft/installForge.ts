5824236df35a8c7cdbad9efbcf21168efe1c04d9
Filip <filip.nowakowski2000@outlook.com>
2022-12-11 02:58:38 +0100

    Skip if mc or forge installed

--- src/api/minecraft/installForge.ts
+++ src/api/minecraft/installForge.ts
@@ -10,7 +10,7 @@ export const installForge = async (
     const versionDir = join(destination, 'versions', `${mcVersion}-forge-${forgeVersion}`)
     if (existsSync(versionDir)) {
         console.log('Forge', forgeVersion, 'installed, skipping...')
-        // return
+        return
     }
 
     const { versions } = await getForgeVersionList({
